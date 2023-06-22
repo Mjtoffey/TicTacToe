0885b8d2ed9300f4cd06c4e1df1a7318a4d72c4a
Mjtoffey <mjtoffey@gmail.com>
2023-06-21 20:31:23 -0400

    updated

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // main.js
+let gameSpace = document.getElementById("gameSpace");
 
-function init(){
-    console.log("hello world!");
+function createBoard() {
+  //create a grid using js to place HTML elements
+  for (var rows = 0; rows <= 3; rows++) {
+    for (var columns = 0; columns <= 3; columns++) {
+      $("#gameSpace").append("<div class='grid'></div>");
+    }
+    $(".grid").width(960 / x);
+    $(".grid").height(960 / x);
+  }
 }
