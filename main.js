4edfe7bf4bb0807b26d176acd3ed650870328134
Mjtoffey <mjtoffey@gmail.com>
2023-06-22 22:45:48 -0400

    updated main.js

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 // main.js
-let gameSpace = document.getElementById("gameSpace");
+let gameSpace = document.getElementById("game");
+generateHTMLElement(obj);
 
-function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    gameSpace.appendChild(cell).className = "grid-item";
+//this funciton allows for the page to create an element on HTML and display it by pulling data from object->rename obj?
+function generateHTMLElement(obj) {
+  let element = document.createElement(obj.type);
+  element.classList = obj.classList;
+  if (obj.id) {
+    element.id = obj.id;
   }
+  if (obj.parent) {
+    obj.parent.appendChild(element);
+  }
+  if (obj.listener) {
+    element.addEventListener("click", obj.listener);
+  }
+  return element;
 }
 
-makeRows(3, 3);
+var obj = {
+  id: "board",
+  type: "div",
+  listener: clickMe,
+  parent: document.getElementById("game"),
+  classList: "container",
+};
+
+function clickMe(e) {}
+
+var winCondition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
